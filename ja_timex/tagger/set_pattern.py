9d494e4ce9dfb546b405a18cd1ffe31b1cf7e3ba
Yuki Okuda <y.okuda@dr-ubie.com>
2021-07-20 08:48:58 +0900

    fix some pattern and add tests

--- ja_timex/tagger/set_pattern.py
+++ ja_timex/tagger/set_pattern.py
@@ -5,14 +5,17 @@ p = Place()
 patterns = []
 
 
-# 基準となる単位より、繰り返しとなる単位が同じかまたは小さくなるようにする
+# 基準となる単位より、繰り返しとなる単位が小さくなるようにする
 #   e.g. 「3ヶ月に1ヶ月」「1ヶ月に1日」は言えるが「1ヶ月に1年」は言えない
+
 # :TODO 基準となる単位の数字より、繰り返しとなる単位の数字が小さくなければいけないが、ルールが複雑で表現が難しい
+# ここではそのバリデーションは行わない
 #   e.g. 「3日に5日」とは言えないが、「3日に5時間」「3日に5回」は言える
 
 # 年
-patterns.append({"pattern": f"{p.range}?年(に)?{p.count}年", "value_template": "P{}Y", "freq_template": "P{}Y"})
-patterns.append({"pattern": f"{p.range}?年(に)?{p.count}[ヶ|か|ケ|箇]?月", "value_template": "P{}Y", "freq_template": "P{}M"})
+patterns.append(
+    {"pattern": f"{p.range}?年(に)?{p.count}[ヶ|か|ケ|箇]?月(に)?", "value_template": "P{}Y", "freq_template": "P{}M"}
+)
 patterns.append({"pattern": f"{p.range}?年(に)?{p.count}週", "value_template": "P{}Y", "freq_template": "P{}W"})
 patterns.append({"pattern": f"{p.range}?年(に)?{p.count}日", "value_template": "P{}Y", "freq_template": "P{}D"})
 patterns.append({"pattern": f"{p.range}?年(に)?{p.count}時間", "value_template": "P{}Y", "freq_template": "PT{}H"})
@@ -21,16 +24,17 @@ patterns.append({"pattern": f"{p.range}?年(に)?{p.count}秒", "value_template"
 patterns.append({"pattern": f"{p.range}?年(に)?{p.count}回", "value_template": "P{}Y", "freq_template": "{}X"})
 
 # 月
-patterns.append({"pattern": f"{p.range}?[ヶ|か|ケ|箇]?月(に)?{p.count}[ヶ|か|ケ|箇]?月", "value_template": "P{}M", "freq_template": "P{}M"})
 patterns.append({"pattern": f"{p.range}?[ヶ|か|ケ|箇]?月(に)?{p.count}週", "value_template": "P{}M", "freq_template": "P{}W"})
 patterns.append({"pattern": f"{p.range}?[ヶ|か|ケ|箇]?月(に)?{p.count}日", "value_template": "P{}M", "freq_template": "P{}D"})
-patterns.append({"pattern": f"{p.range}?[ヶ|か|ケ|箇]?月(に)?{p.count}時間", "value_template": "P{}M", "freq_template": "PT{}H"})
+patterns.append(
+    {"pattern": f"{p.range}?[ヶ|か|ケ|箇]?月(に)?{p.count}時間", "value_template": "P{}M", "freq_template": "PT{}H"}
+)
 patterns.append({"pattern": f"{p.range}?[ヶ|か|ケ|箇]?月(に)?{p.count}分", "value_template": "P{}M", "freq_template": "PT{}M"})
 patterns.append({"pattern": f"{p.range}?[ヶ|か|ケ|箇]?月(に)?{p.count}秒", "value_template": "P{}M", "freq_template": "PT{}S"})
 patterns.append({"pattern": f"{p.range}?[ヶ|か|ケ|箇]?月(に)?{p.count}回", "value_template": "P{}M", "freq_template": "{}X"})
 
 # 週
-patterns.append({"pattern": f"{p.range}?週(に)?{p.count}週", "value_template": "P{}W", "freq_template": "P{}W"})
+patterns.append({"pattern": f"{p.range}?週に{p.count}週", "value_template": "P{}W", "freq_template": "P{}W"})
 patterns.append({"pattern": f"{p.range}?週(に)?{p.count}日", "value_template": "P{}W", "freq_template": "P{}D"})
 patterns.append({"pattern": f"{p.range}?週(に)?{p.count}時間", "value_template": "P{}W", "freq_template": "PT{}H"})
 patterns.append({"pattern": f"{p.range}?週(に)?{p.count}分", "value_template": "P{}W", "freq_template": "PT{}M"})
@@ -38,32 +42,26 @@ patterns.append({"pattern": f"{p.range}?週(に)?{p.count}秒", "value_template"
 patterns.append({"pattern": f"{p.range}?週(に)?{p.count}回", "value_template": "P{}W", "freq_template": "{}X"})
 
 # 日
-patterns.append({"pattern": f"{p.range}?日(に)?{p.count}日", "value_template": "P{}D", "freq_template": "P{}D"})
+patterns.append({"pattern": f"{p.range}?日に{p.count}日", "value_template": "P{}D", "freq_template": "P{}D"})
 patterns.append({"pattern": f"{p.range}?日(に)?{p.count}時間", "value_template": "P{}D", "freq_template": "PT{}H"})
 patterns.append({"pattern": f"{p.range}?日(に)?{p.count}分", "value_template": "P{}D", "freq_template": "PT{}M"})
 patterns.append({"pattern": f"{p.range}?日(に)?{p.count}秒", "value_template": "P{}D", "freq_template": "PT{}S"})
 patterns.append({"pattern": f"{p.range}?日(に)?{p.count}回", "value_template": "P{}D", "freq_template": "{}X"})
 
 # 時間
-patterns.append({"pattern": f"{p.range}?時間(に)?{p.count}時間", "value_template": "PT{}H", "freq_template": "PT{}H"})
+patterns.append({"pattern": f"{p.range}?時間に{p.count}時間", "value_template": "PT{}H", "freq_template": "PT{}H"})
 patterns.append({"pattern": f"{p.range}?時間(に)?{p.count}分", "value_template": "PT{}H", "freq_template": "PT{}M"})
 patterns.append({"pattern": f"{p.range}?時間(に)?{p.count}秒", "value_template": "PT{}H", "freq_template": "PT{}S"})
 patterns.append({"pattern": f"{p.range}?時間(に)?{p.count}回", "value_template": "P{}H", "freq_template": "{}X"})
 
 # 分
-patterns.append({"pattern": f"{p.range}?分(に)?{p.count}分", "value_template": "PT{}M", "freq_template": "PT{}M"})
-patterns.append({"pattern": f"{p.range}?分(に)?{p.count}秒", "value_template": "PT{}M", "freq_template": "PT{}S"})
-patterns.append({"pattern": f"{p.range}?分(に)?{p.count}回", "value_template": "PT{}M", "freq_template": "{}W"})
+patterns.append({"pattern": f"{p.range}?分(間)?に{p.count}分", "value_template": "PT{}M", "freq_template": "PT{}M"})
+patterns.append({"pattern": f"{p.range}?分(間)?に{p.count}秒", "value_template": "PT{}M", "freq_template": "PT{}S"})
+patterns.append({"pattern": f"{p.range}?分(間)?に{p.count}回", "value_template": "PT{}M", "freq_template": "{}W"})
 
 # 秒
-patterns.append({"pattern": f"{p.range}?秒間に?{p.count}秒", "value_template": "PT{}S", "freq_template": "PT{}S"})
-patterns.append({"pattern": f"{p.range}?秒間に?{p.count}回", "value_template": "PT{}S", "freq_template": "{}X"})
-
-
-# 年100日
-# 年間100回
-# 年間/月間/
-# 半日
+patterns.append({"pattern": f"{p.range}?秒(間)?に{p.count}秒", "value_template": "PT{}S", "freq_template": "PT{}S"})
+patterns.append({"pattern": f"{p.range}?秒(間)?に{p.count}回", "value_template": "PT{}S", "freq_template": "{}X"})
 
 
 # quant:EACH
@@ -77,4 +75,4 @@ patterns.append({"pattern": f"毎年", "value": "P1Y", "quant": "EACH"})
 # patterns.append({"pattern": f"毎{p.count}?(回|日|時間)", "quant": ""})
 
 # quant:EVERY
-patterns.append({"pattern": f"毎{p.count}?(回|日|時間)", "quant": ""})
+patterns.append({"pattern": f"毎{p.count}?(回|日|時間)", "quant": "EVERY"})
--- tests/test_set_tagger.py
+++ tests/test_set_tagger.py
@@ -38,6 +38,77 @@ def test_count(t):
     assert t.parse("週40時間").freq == "PT40H"
 
 
+def test_count_range_is_temporal_expression(t):
+    # 時間表現を対象としており、「3回に1回」のような時間を含まない表現は解析しない
+    assert t.parse("3回に1回") is None
+    assert t.parse("1回1時間") is None
+
+
+def test_count_kanni_in_minutes_and_second(t):
+    # 「に」「間に」とは言う
+    assert t.parse("1秒に1回") is not None
+    assert t.parse("1秒間に1回") is not None
+    assert t.parse("1分に1回") is not None
+    assert t.parse("1分間に1回") is not None
+
+    # 「間」だけでは言わない
+    assert t.parse("1秒間1回") is None
+    assert t.parse("1分間1回") is None
+
+
+def test_count_larger_than_range_is_invalid(t):
+    # 基準となる単位より、繰り返しとなる単位が大きくなってはいけない
+    assert t.parse("1ヶ月に1年") is None
+    assert t.parse("1週に1年") is None
+    assert t.parse("1週に1ヶ月") is None
+    assert t.parse("1日に1年") is None
+    assert t.parse("1日に1ヶ月") is None
+    assert t.parse("1日に1週") is None
+    assert t.parse("1時間に1年") is None
+    assert t.parse("1時間に1ヶ月") is None
+    assert t.parse("1時間に1週") is None
+    assert t.parse("1時間に1日") is None
+    assert t.parse("1分に1年") is None
+    assert t.parse("1分に1ヶ月") is None
+    assert t.parse("1分に1週") is None
+    assert t.parse("1分に1日") is None
+    assert t.parse("1分に1時間") is None
+    assert t.parse("1秒に1年") is None
+    assert t.parse("1秒に1月") is None
+    assert t.parse("1秒に1週") is None
+    assert t.parse("1秒に1日") is None
+    assert t.parse("1秒に1時間") is None
+    assert t.parse("1秒に1分") is None
+
+
+def test_same_count_and_range_is_invalid_at_day_expression(t):
+    # 基準となる単位と繰り返しとなる単位が同じでかつ対象が年や月の場合は対象としない
+    assert t.parse("4年に1年") is None
+    assert t.parse("3ヶ月に1月") is None
+
+
+def test_same_count_and_range_is_valid_at_time_expression(t):
+    # 基準となる単位と繰り返しとなる単位が同じでかつ対象が時間や週の場合は対象とする
+    assert t.parse("3週に1週") is not None  # e.g. 土曜日は3週に1週、出勤してください
+    assert t.parse("7日に1日") is not None  # e.g. 7日間に1日は休まねばなりません
+    assert t.parse("3時間に1時間") is not None
+    assert t.parse("3分間に1分") is not None
+    assert t.parse("3秒に1秒") is not None
+
+
+def test_count_has_ni_as_same_unit(t):
+    # 頻度表現で「3日1日」とは言わない。必ず「3日に1日」というように「に」が含まれる
+    assert t.parse("3週に1週") is not None
+    assert t.parse("3日に1日") is not None
+    assert t.parse("3分に1分") is not None
+    assert t.parse("3秒に1秒") is not None
+
+    assert t.parse("3週1週") is None
+    assert t.parse("3日1日") is None
+    assert t.parse("3分1分") is None
+    assert t.parse("3秒1秒") is None
+
+
 def test_count_range(t):
     assert t.parse("3年に1回").value == "P3Y"
     assert t.parse("3年に1回").freq == "1X"
