4c579a7865bad9370f6c797ffdd7771cf118054a
Yuki Okuda <y.okuda@dr-ubie.com>
2021-08-06 22:07:24 +0900

    update to_datetime and to_duration

--- tools/timex_checker.py
+++ tools/timex_checker.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pendulum
 import streamlit as st
 
 from ja_timex.timex import TimexParser
@@ -14,7 +15,29 @@ def export_as_tagged_text(timexes, text):
 st.header("ja_timex Checker")
 text = st.text_area("Text to parse", "")
 
-timex_parser = TimexParser()
+use_reference = st.sidebar.checkbox("Use reference")
+if use_reference:
+    reference_year = st.sidebar.number_input("year", min_value=1, value=2021)
+    reference_month = st.sidebar.number_input("month", min_value=1, max_value=12, value=7)
+    reference_day = st.sidebar.number_input("day", min_value=1, max_value=31, value=18)
+    reference_hour = st.sidebar.number_input("hour", min_value=0, max_value=30, value=10)
+    reference_minute = st.sidebar.number_input("minute", min_value=0, max_value=60, value=00)
+    reference_second = st.sidebar.number_input("second", min_value=0, max_value=60, value=00)
+
+if use_reference:
+    reference_datetime = pendulum.datetime(
+        year=reference_year,
+        month=reference_month,
+        day=reference_day,
+        hour=reference_hour,
+        minute=reference_minute,
+        second=reference_second,
+        tz="Asia/Tokyo",
+    )
+    timex_parser = TimexParser(reference=reference_datetime)
+    st.sidebar.write(reference_datetime)
+else:
+    timex_parser = TimexParser()
 
 if text:
     text = text.replace("\n", "").strip()
@@ -44,3 +67,8 @@ if text:
             )
             st.subheader(f"{timex.tid}: {timex.text}")
             st.table(timex_df)
+
+            st.subheader("to_datetime")
+            st.write(timex.to_datetime())
+            st.subheader("to_duration")
+            st.write(timex.to_duration())
