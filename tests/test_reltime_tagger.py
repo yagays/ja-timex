b1ae24aed1906467946832ec66530dc24cefc1cf
Yuki Okuda <y.okuda@dr-ubie.com>
2021-08-08 21:11:52 +0900

    update half expression

--- tests/test_reltime_tagger.py
+++ tests/test_reltime_tagger.py
@@ -152,3 +152,20 @@ def test_word_now(t):
 
     assert t.parse("今年").value == "P0Y"
     assert t.parse("今年").mod == "NOW"
+
+
+def test_half_suffix_reltime(t):
+    assert t.parse("1時間半後").value == "PT1.5H"
+    assert t.parse("1時間半後").mod == "AFTER"
+    assert t.parse("1分半後").value == "PT1.5M"
+    assert t.parse("1秒半後").value == "PT1.5S"
+
+    assert t.parse("1年半後").value == "P1.5Y"
+    assert t.parse("1ヶ月半後").value == "P1.5M"
+    assert t.parse("1週間半後").value == "P1.5W"
+    assert t.parse("1週半後").value == "P1.5W"
+    assert t.parse("1日半後").value == "P1.5D"
+
+    assert t.parse("1時間半ほど後").value == "PT1.5H"
+    assert t.parse("1時間半くらい後").value == "PT1.5H"
+    assert t.parse("1時間半近く後").value == "PT1.5H"
--- tests/test_timex.py
+++ tests/test_timex.py
@@ -84,6 +84,20 @@ def test_morning_evening(p):
     assert timexes[0].text == "今夜9時"
 
 
+def test_duration_with_half_expression(p):
+    timexes = TimexParser().parse("今から1時間半後に始めます")
+    assert len(timexes) == 1
+    assert timexes[0].value == "PT1.5H"
+    assert timexes[0].type == "DURATION"
+    assert timexes[0].text == "1時間半後"
+
+    timexes = TimexParser().parse("今から2年半ほど前の話")
+    assert len(timexes) == 1
+    assert timexes[0].value == "P2.5Y"
+    assert timexes[0].type == "DURATION"
+    assert timexes[0].text == "2年半ほど前"
+
+
 def test_reference_datetime_without_reference(p):
     timexes = p.parse("2021年7月18日")
     assert timexes[0].reference is None
